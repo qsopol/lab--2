95328eff05e1c4aa5d04efc49a1725210e2949b9
qsopol <byqsopol@gmail.com>
2023-12-24 19:13:09 +0200

    Create calculator.h

---
+++ calculator.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+	public:
+        int Add(int a, int b);
+        int Mul(double, double);
+        int Sub(int a, int b);
+};
+
+#endif//CALCULATOR_H
